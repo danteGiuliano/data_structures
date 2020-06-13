3217af7aff7689534a72f68db8a3e564ea5c2401
Dan <Dan@DESKTOP-MN2PFLV>
2020-06-12 23:31:44 -0300

    en progreso.

--- src/estructuras/conjuntistas/ArbolAVL.java
+++ src/estructuras/conjuntistas/ArbolAVL.java
@@ -17,27 +17,40 @@ public class ArbolAVL {
         this.raiz = null;
     }
 
+    /**
+     * La insercion deberia ser de orden O(log n) trato de no hacer llamadas
+     * extras usando nulls, para hacer un algoritmo mas centrado a las
+     * rotaciones. si el elemento esta repetido, debe retornar false.
+     *
+     * @param elem un elemento comparable
+     * @return True si el elemento es unico, false si esta repetido.
+     */
     public boolean insertar(Comparable elem) {
         boolean flag = true;
         if (this.raiz == null) {
             this.raiz = new NodoAVL(elem);
         } else {
-            flag=insertarAux(this.raiz, elem, null);
+            flag = insertarAux(this.raiz, elem, null);
         }
         return flag;
     }
 
     private boolean insertarAux(NodoAVL hijo, Comparable elem, NodoAVL padre) {
         boolean flag = false;
-        if (!hijo.getElemento().equals(elem)) {
-            int comparar = hijo.getElemento().compareTo(elem);
-            if (comparar == 0) {
-
-            } else if (comparar > 0 && hijo.getDerecha() != null) {
-                flag=insertarAux(hijo.getDerecha(), elem, hijo);
+        //Por esto evito llamadas extras, para evitar una sentencia sobrecargada
+        int comparar = hijo.getElemento().compareTo(elem);
+        if (comparar != 0) {
+            if (comparar > 0 && hijo.getDerecha() != null) {
+                flag = insertarAux(hijo.getDerecha(), elem, hijo);
             } else {
-                if (hijo.getIzquierda() != null) {
-                    flag=insertarAux(hijo.getIzquierda(), elem, hijo);
+                if (hijo.getDerecha() == null) {
+                    hijo.setDerecho(new NodoAVL(elem));
+                    hijo.recalcularAltura();
+                } else {
+                    if (hijo.getIzquierda() != null) {
+                        flag = insertarAux(hijo.getIzquierda(), elem, hijo);
+                    }
+
                 }
             }
         }
