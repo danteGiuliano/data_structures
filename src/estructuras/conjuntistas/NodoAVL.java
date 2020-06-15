0712af67401b45a919e687a442ab4bdee2edc524
Dan <Dan@DESKTOP-MN2PFLV>
2020-06-15 17:32:48 -0300

    Ahora NodoAVL verifica bien modificacion en recalcularAltura y constructor

--- src/estructuras/conjuntistas/NodoAVL.java
+++ src/estructuras/conjuntistas/NodoAVL.java
@@ -9,57 +9,65 @@ package estructuras.conjuntistas;
  *
  * @author Dan
  */
- class NodoAVL {
+class NodoAVL {
+
     private Comparable elemento;
     private int altura;
     private NodoAVL izquierdo;
     private NodoAVL derecho;
-    public NodoAVL(Comparable elemento,NodoAVL izq,NodoAVL der){
-        //Si se ingresa un null como hijo derecho o izquierdo va a tomar 
-       // una altura erronea
-        this.izquierdo=izq;
-        this.derecho=der;
-        this.elemento=elemento;
-        this.altura=0;
+
+    public NodoAVL(Comparable elemento, NodoAVL izq, NodoAVL der) {
+        this.izquierdo = izq;
+        this.derecho = der;
+        this.elemento = elemento;
+        this.recalcularAltura();
     }
-    public NodoAVL(Comparable elemento){
-        this.elemento=elemento;
-        this.altura=0;
+
+    public NodoAVL(Comparable elemento) {
+        this.elemento = elemento;
+        this.izquierdo = null;
+        this.derecho = null;
+        this.altura = 0;
     }
-    public int getAltura(){
+
+    public int getAltura() {
         return this.altura;
     }
-    public Comparable getElemento(){
+
+    public Comparable getElemento() {
         return this.elemento;
     }
-    public NodoAVL getIzquierda(){
+
+    public NodoAVL getIzquierda() {
         return this.izquierdo;
     }
-    public NodoAVL getDerecha(){
+
+    public NodoAVL getDerecha() {
         return this.derecho;
     }
-    public void setDerecho(NodoAVL der){
-        this.derecho=der;
+
+    public void setDerecho(NodoAVL der) {
+        this.derecho = der;
     }
-    public void setIzquierdo(NodoAVL izq){
-        this.izquierdo=izq;
+
+    public void setIzquierdo(NodoAVL izq) {
+        this.izquierdo = izq;
     }
-    public void setElemento(Comparable elem){
+
+    public void setElemento(Comparable elem) {
         //Si seteo un nulo como elemento el tda se rompera. ya que la altura
         //deberia ser -1 (Nodo vacio) pero puede tener hijos .
-        this.elemento=elem;
+        this.elemento = elem;
     }
-    public void recalcularAltura(){
-        if(this.derecho!=null||this.izquierdo!=null){
-        int altD=0,altI=0;     
-        if(this.derecho!=null){
-            altD=this.derecho.getAltura();
+
+    public void recalcularAltura() {
+        int altD = -1, altI = -1;
+        if (this.derecho != null) {
+            altD = this.derecho.getAltura();
         }
-        if(this.izquierdo!=null){
-            altI=this.getAltura();
+        if (this.izquierdo != null) {
+            altI = this.izquierdo.getAltura();
         }
-        this.altura=Math.max(altD,altI)+1;
-    }
-      
+        this.altura = Math.max(altI, altD) + 1;
     }
 }
